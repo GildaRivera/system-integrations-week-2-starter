ee44d46296e923e28da0883c520a560230797813
gildarivera <gilda.rivera@telus.com>
2022-04-08 16:09:08 -0600

    Feat: Adding socket io

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const app = express();
 const server = require("http").createServer(app);
 
 const content = require("./content/cards.json");
-
+const { Server } = require("socket.io");
+const io = new Server(server);
 // Middlewares
 
 app.use(express.static("public"));
@@ -17,7 +18,15 @@ app.set("view engine", "ejs");
 app.get("/", (req, res) => {
   res.render("index", { content });
 });
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
 
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+});
 // Starting server.
 
 server.listen(3000, () => {
