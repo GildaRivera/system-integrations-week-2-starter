6971dd8e5adbc40ebe0a16cdc43f23fe6667c114
gildarivera <gilda.rivera@telus.com>
2022-04-11 15:03:13 -0600

    fix: Changing config

--- views/chat.js
+++ views/chat.js
@@ -1,5 +1,41 @@
-function Hello() {
-    return <h1>Hello World!</h1>;
-  }
+var socket = io();
+var messages = document.getElementById('messages');
+var form = document.getElementById('form');
+var input = document.getElementById('input');
+var deleteB = document.getElementById('delete')
 
-  ReactDOM.render(<Hello />, document.getElementById('body'))
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  if (input.value) {
+    socket.emit('chat message', {"user":1, "message":input.value});
+    input.value = '';
+  }
+});
+deleteB.addEventListener('click', function (e) {
+  e.preventDefault();
+  socket.emit('delete message', input.value);
+
+});
+window.addEventListener('beforeunload', function (e) {
+  e.preventDefault();
+  e.returnValue = '';
+
+  socket.emit('disconnect', 1)
+});
+socket.on('chat message', function (msg) {
+  var item = document.createElement('li');
+  item.textContent = msg.message;
+  messages.appendChild(item);
+  window.scrollTo(0, document.body.scrollHeight);
+});
+
+socket.on('delete message', function (msg) {
+  messages.innerHTML = ""
+})
+socket.on('disconnect', function () {
+  socket.disconnect();
+});
+
+socket.on('user', function (data) {
+  document.getElementById('userName').innerHTML=data
+})
