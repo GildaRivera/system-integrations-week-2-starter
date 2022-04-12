8bcc12bc2303928d5e7c6c9ce1b42dadc119122d
gildarivera <gilda.rivera@telus.com>
2022-04-12 09:38:58 -0600

    feat: Adding tests

---
+++ services/messages.test.js
@@ -0,0 +1,21 @@
+const Message = require('./messages');
+
+test('addMessage should add new message', () => {
+  Message.addMessage( {user: "user1", message: "m1" });
+  expect(Message.getMessage().length).toBe(1);
+});
+
+test('getMessage should return an array', () => {
+    Message.addMessage( {user: "user1", message: "m1" });
+    Message.addMessage( {user: "user1", message: "m1" });
+    let arr=Message.getMessage()
+   expect(arr.length).toBeGreaterThan(1)
+ });
+ test('deleteMessage should return a smaller array', () => {
+    Message.addMessage( {user: "user1", message: "m1" });
+    Message.addMessage( {user: "user1", message: "m1" });
+    Message.addMessage( {user: "user1", message: "m1" });
+    Message.deleteMessage()
+    let arr=Message.getMessage()
+   expect(arr.length).toBe(0);
+ })
---
+++ services/users.test.js
@@ -0,0 +1,26 @@
+const User = require('./users');
+describe('user test',()=>{
+
+
+test('addUser should add new user', () => {
+   User.addUser( {id: 1, name: "name" });
+  expect(User.getUser().length).toBe(1);
+});
+
+test('getUser should return an array', () => {
+    User.addUser( {id: 1, name: "name" });
+    User.addUser( {id: 2, name: "name" });
+    let arr=User.getUser();
+   expect(arr.length).toBeGreaterThan(1)
+ });
+
+
+ test('deleteUser should return a smaller array', () => {
+    User.addUser( {id: 1, name: "name" });
+    User.addUser( {id: 2, name: "name" });
+    User.addUser( {id: 3, name: "name" });
+    User.deleteUser()
+    let arr=User.getUser();
+   expect(arr.length).toBe(6);
+ })
+})
